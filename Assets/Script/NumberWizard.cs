a8a774c8a72b199592e6c150a38b06ecf15a112c
IFA <imamfh24@gmail.com>
2020-07-20 23:48:57 +0700

    First Commit

---
+++ Assets/Script/NumberWizard.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    /*
+     
+    void Start() merupakan sebuah method/fungsi yang DIJALANKAN PERTAMA KALI ketika script/game dijalankan atau sebelum frame pertama game dijalankan
+    method ini biasa disebut inisialisasi pertama sebelum game dijalankan
+    void Start() hanya dijalankan sekali saja ketika game dijalankan pertama kali
+    
+     */
+
+    [SerializeField] int max, min;
+    /*[SerializeField] Text textGuess;*/
+    [SerializeField] TextMeshProUGUI textGuess;
+    int guess;
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        nextGuess();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void nextGuess()
+    {
+        guess = Random.Range(min, max + 1);
+        displayGuess();
+    }
+
+    public void onPressHigher()
+    {
+        min = guess + 1;
+        nextGuess();
+    }
+
+    public void onPressLower()
+    {
+        max = guess - 1;
+        nextGuess();
+    }
+    
+    public void displayGuess()
+    {
+        textGuess.text = guess.ToString();
+    }
+}
---
+++ Assets/Script/SceneLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void loadStartScene()
+    {
+        //SceneManager.LoadScene(dalam ini) bisa berupa index ataupun nama dari scene
+        SceneManager.LoadScene(0);
+    }
+
+    public void loadNextScene()
+    {
+        // Variabel ini digunakan untuk menampung index yang mana saat ini scene yang sedang aktif/dibuka/sedang dimainkan
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void loadPreviousScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex - 1);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
